194f9f396f8ea55977157085c2dabe25cf65dde3
anupriyadhiman <anupriyadhiman0605@gmail.com>
2023-02-22 01:29:56 +0530

    Assignment 2

--- Assignment_2_Production_Model_Package/tests/test_prediction.py
+++ Assignment_2_Production_Model_Package/tests/test_prediction.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 from sklearn.metrics import accuracy_score
 
-from regression_model.predict import make_prediction
+from classification_model.predict import make_prediction
 
 
 def test_make_prediction(sample_input_data):
